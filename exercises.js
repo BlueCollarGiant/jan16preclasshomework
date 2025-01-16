73023f309ada2277aabb87fefc0c54fad2020a99
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-01-16 14:50:06 -0600

    finished pre work for class

--- exercises.js
+++ exercises.js
@@ -1,6 +1,46 @@
-let originalPrice = 50;
+/* let originalPrice = 50;
 let discount = originalPrice * 0.2;
 
 let finalPrice = originalPrice - discount;
 
-console.log(finalPrice)
+console.log(finalPrice) */
+/* end of first short exercise */
+/* start of second exercise */
+
+/* let price = 100;
+const taxRate = 0.1;
+const discount = 0.15;
+
+let priceWithtax = price + (price * taxRate);
+let total = priceWithtax - (priceWithtax * discount);
+
+console.log(total) */
+/* end of second exercise */
+/* start of third excersise */
+
+/* let firstName = "Jane ";
+let lastName = "Smith";
+let greeting = "Welcome to the platform! ";
+
+let message = `${greeting}`+ `${firstName}` + `${lastName}`;
+
+console.log(message)*/
+/* this ends the third excersise */
+/* this starts the fourth */
+/* const name = 'Alice';
+const favoriteColor = 'blue';
+const age = 25;
+
+let greeting = `Hello ${name} so glad to see you! i hear you're turning ${age} today! we know your favorite color is ${favoriteColor} so we got you a cake that color!`;
+
+console.log(greeting) */
+/* this ends the fourth*/
+/* this starts the fifth*/
+
+let storeInventory = 100;
+let itemsSold = 15;
+let stockRefill = 10;
+
+let finalStock = (storeInventory - itemsSold) + stockRefill;
+
+console.log(finalStock)
